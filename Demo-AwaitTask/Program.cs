0b5d74129deac9de38d8ad4422c61cd45f65cb1d
xavierchen2015@gmail.com <xavierchen2015@gmail.com>
2020-02-09 18:46:08 +0800

    加入專案檔案。

---
+++ Demo-AwaitTask/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using ConsoleDump;
+
+namespace Demo_AwaitTask
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ////Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            ////Task.Run(() => Task1());
+            ////Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            ////Console.WriteLine("Task22");
+            //Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            //Console.WriteLine(DateTime.Now);
+            //TplExample tpl = new TplExample();
+            //Console.WriteLine("Completed start");
+            //var rep2 =  tpl.getClient("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=1f1aaba5-616a-4a33-867d-878142cac5c4");
+
+            //Console.WriteLine(tpl.gogo());
+            //Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            ////tpl.Task1();
+            ////var task2 = tpl.Task2();
+            //Console.WriteLine("Completed task");
+            //Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            ////await task2;
+            ////var rep = await tpl.getClient("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=190796c8-7c56-42e0-8068-39242b8ec927");
+
+            ////Console.WriteLine(rep.Dump<String>());
+            ////Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            ////await task2;
+            
+            Console.WriteLine($"Main  {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine(DateTime.Now);
+            TplExample tpl = new TplExample();
+            Console.WriteLine("Start Cooking !!!");
+
+            var makecoffee = await tpl.MakeCoffee();
+            Console.WriteLine("making coffee......");
+
+            var grilledtoast =  tpl.GrilledToast();
+            Console.WriteLine("grilling toast......");
+
+            
+
+            var fryegg = tpl.FryEgg();
+            Console.WriteLine("frying egg......");
+
+            var frysteak = tpl.FrySteak();
+            Console.WriteLine("frying steak......");
+            await frysteak;
+            await grilledtoast;
+            Console.WriteLine("return  grilledtoast ");
+            int result = await fryegg;
+            Console.WriteLine(result);
+            
+            
+            //await makecoffee;
+
+            Console.WriteLine("Completed !!!");
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+        }
+
+        //static async void Task1()
+        //{
+        //    TplExample tpl = new TplExample();
+        //    var task1 = tpl.Task1();
+        //    var task2 = tpl.Task2();
+        //    Console.WriteLine("Completed task");
+        //    await task2;
+        //    await task1;
+
+        //    Task.Delay(1000).Wait();
+        //    Console.WriteLine("Task11");
+        //}
+
+    }
+}
---
+++ Demo-AwaitTask/TplExample.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Http;
+using System.IO;
+
+namespace Demo_AwaitTask
+{
+    public class TplExample
+    {
+        public async void Task1()
+        {
+            Console.WriteLine($"Task1 : {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(4000);
+            Console.WriteLine($"Task1 : {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Task1");
+        }
+
+        public async Task Task2()
+        {
+            Console.WriteLine($"Task2 : {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(2000);
+            Console.WriteLine($"Task2 : {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Containue from Task1");
+        }
+
+        public string gogo()
+        {
+            return "gogo";
+        }
+
+        public async Task<String> getClient(string url)
+        {
+            await Task.Delay(10000);
+            HttpClient _client = new HttpClient();
+            
+            HttpResponseMessage rep = await _client.GetAsync(url);
+            rep.Content.Headers.ContentType.CharSet = "utf-8";
+            string responseBody = await rep.Content.ReadAsStringAsync();
+            var byteArray = rep.Content.ReadAsByteArrayAsync().Result;
+            var result = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+            Console.WriteLine(url);
+            Console.WriteLine($"getClient : {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(2000);
+            using (StreamWriter sw = new StreamWriter("TestFile.TXT"))   //小寫TXT     
+            {
+                sw.Write(result);
+            }
+            return result;
+        }
+
+        public async Task<int> GrilledToast()
+        {
+            Console.WriteLine("=== Grilleding Toast ===");
+            await Task.Delay(3000);
+            Console.WriteLine($"MakeCoffee ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("=== Grilleding Toast done ===");
+            return 0;
+        }
+
+        public async Task<int> FryEgg()
+        {
+            Console.WriteLine("=== Frying Egg ===");
+            await Task.Delay(2000);
+            Console.WriteLine($"MakeCoffee ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("=== Frying Egg done ===");
+            return 0;
+        }
+
+        public async Task<int> MakeCoffee()
+        {
+            Console.WriteLine("=== Making Coffee ===");
+            await Task.Delay(20000);
+            Console.WriteLine($"MakeCoffee ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("=== Making Coffee done ===");
+            return 0;
+        }
+        public async Task<int> FrySteak()
+        {
+            Console.WriteLine("=== Frying Steak ===");
+            await Task.Delay(8000);
+            Console.WriteLine($"FrySteak ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("=== Frying Steak done ===");
+            return 0;
+        }
+    }
+}
